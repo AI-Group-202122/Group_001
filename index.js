a14654f0c413d667fd7860898bc9d61eb3eecd76
TejasRanjith <tejasranjith035611@gmail.com>
2020-08-01 11:39:12 +0400

    A(index.html , index.js) D(a) M(null.null(0)) U(index.html , index.js)

---
+++ index.js
@@ -0,0 +1,12 @@
+let person = {
+    name : "Tejas",
+    age : 30
+}
+console.log(person.name)
+console.log(person.age)
+
+function greet(name) {
+    console.log("Hello "+ name)
+}
+
+greet ("Tejas")
